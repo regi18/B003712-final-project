e29f681b3d735229bcee9b5ef31fd22c0cbf7885
Simone Carletti <simone.carletti@genomedics.it>
2023-06-21 13:07:27 +0200

    wip

--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,33 +1,36 @@
 <template>
+  <h2 style="padding-top: 0.4em">Manage the gazette</h2>
+
   <div class="container admin-container">
-
+    <!-- TODO move somewhere in mobile -->
     <div class="sidenav">
-
-      TEST
+      <ul>
+        <li @click="section = 'articles'">Articles</li>
+        <li @click="section = 'cartoons'">Cartoons</li>
+        <li @click="section = 'research-journal'">The Research Journal</li>
+      </ul>
     </div>
 
     <div class="content">
-      TEST
+      <!-- List with CRUD operations -->
+      {{ section }}
     </div>
-
   </div>
 </template>
 
 <script lang="ts">
-import { RouterLink } from 'vue-router';
-
 // TODO protect this component/route from unauthenticated users
 export default {
   name: 'AdminView',
-  components: {
-    RouterLink,
-  },
+  components: {},
   created() {
     // TODO
   },
   methods: {},
   data() {
-    return {};
+    return {
+      section: 'articles',
+    };
   },
 };
 </script>
@@ -37,5 +40,23 @@ export default {
   min-height: 50vh;
   display: grid;
   grid-template-columns: 20% auto;
+  max-width: 1000px;
+}
+
+.sidenav {
+  li {
+    list-style-type: none;
+    cursor: pointer;
+    padding: 0.5em 0;
+    border-bottom: 1px solid #ccc;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+
+.content {
+  padding: 1em 2.5em;
 }
 </style>

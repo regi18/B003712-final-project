67d95169e5284b3475f6ad6a4ba5b75899f00c15
Simone Carletti <regi18@outlook.it>
2023-07-18 16:17:12 +0200

    fix

--- frontend/src/services/LoginService.ts
+++ frontend/src/services/LoginService.ts
@@ -1,12 +1,14 @@
 import router from '@/router';
 import { post, get } from './AjaxService';
 
+
 export default class LoginService {
   static async login(username: string, password: string) {
     const res = await post('/login', { username, password });
     localStorage.setItem('token', res?.token);
 
-    router.push('/admin');
+    await router.push('/admin');
+    router.go(0);
   }
 
   static isLoggedIn() {
@@ -16,6 +18,7 @@ export default class LoginService {
   static async logout() {
     await get('/logout');
     localStorage.removeItem('token');
-    router.push('/');
+    await router.push('/');
+    router.go(0);
   }
 }
